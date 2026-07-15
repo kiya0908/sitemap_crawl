e2130e6eee330acabc04070f79a98c7c9d8f7c66
kiya0908 <2015189466@qq.com>
2026-07-15 16:47:16 +0800

    chore: add Worker environment types

---
+++ worker-configuration.d.ts
@@ -0,0 +1,8 @@
+interface Env {
+  DB: D1Database
+  OPENROUTER_API_KEY?: string
+  OPENROUTER_MODEL: string
+  OPENROUTER_SITE_URL: string
+  OPENROUTER_APP_NAME: string
+  AI_PROMPT_VERSION: string
+}
